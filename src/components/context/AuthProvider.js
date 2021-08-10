b0ec25f75d5efb7f9cd3e72a8cb3688ef73b36dc
dovekush sharma <dovekush19@gmail.com>
2021-08-10 16:29:53 +0530

    added auth

--- src/components/context/AuthProvider.js
+++ src/components/context/AuthProvider.js
@@ -1,17 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { fbApp } from '../../utils/fbApp';
 export const AuthContext=React.createContext();
+
 function AuthProvider(props) {
-    const [currentUser,setCurrentUser]=useState();
-    
+    const [currentUser,setCurrentUser]=useState(null);
+    const [loading,setLoading]=useState(true);
     function signUp(email,password){
-       
+      return  fbApp.auth().createUserWithEmailAndPassword(email,password);
     }
     function logOut(){
-
+     return fbApp.auth().signOut();
     }
     function logIn(email,password){
-
+      return fbApp.auth().signInWithEmailAndPassword(email,password);
     }
+    useEffect(()=>{
+        let unsub=fbApp.auth().onAuthStateChanged((user)=>{
+            setCurrentUser(user);
+            setLoading(false);
+        })
+        return unsub;
+    })
     let value={
         currentUser,
         signUp,
@@ -20,7 +29,7 @@ function AuthProvider(props) {
     }
     return (
         <AuthContext.Provider value={value}>
-            {props.children}
+            {!loading&&props.children}
         </AuthContext.Provider>
     )
 }
